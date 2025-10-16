a609a48ea8067b1e06380883a06c1ba9ecef8e35
moon-26k <moonkarmakar2609@gmail.com>
2025-10-16 18:31:30 +0530

    Add files via upload

---
+++ MeasurementConversion.py
@@ -0,0 +1,9 @@
+inch=float(input("enter inches:"))
+feet=inch/12
+yards=inch/36
+cm=inch*2.54
+m=inch/39.37
+print(" in feet:",feet)
+print(" in Yards:",yards)
+print(" in centimeter",cm)
+print(" in meters:",m)
